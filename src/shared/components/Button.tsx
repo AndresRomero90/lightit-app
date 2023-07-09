bba75ab0bfbf614505af0a8763c9b0b823c259a5
Andres Romero <andres.romero.90@hotmail.com>
2023-07-09 19:07:50 -0300

    ⚡️ Updated Input and Button components

--- src/shared/components/Button.tsx
+++ src/shared/components/Button.tsx
@@ -6,15 +6,29 @@ type Props = {
   type?: ButtonHTMLAttributes<HTMLButtonElement>['type'];
   onClick?: () => void;
   className?: string;
+  disabled?: boolean;
 };
 
-const Button: React.FC<Props> = ({ label, type, onClick, className }) => {
+const Button: React.FC<Props> = ({
+  label,
+  type,
+  onClick,
+  disabled,
+  className,
+}) => {
   const buttonClasses = clsx(
     'rounded bg-blue-900 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-600 active:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600',
-    className
+    className,
+    {
+      'bg-gray-400 hover:bg-gray-400 hover:cursor-not-allowed': disabled,
+    }
   );
   return (
-    <button type={type} onClick={onClick} className={buttonClasses}>
+    <button
+      disabled={disabled}
+      type={type}
+      onClick={onClick}
+      className={buttonClasses}>
       {label}
     </button>
   );
--- src/shared/components/Input.tsx
+++ src/shared/components/Input.tsx
@@ -38,7 +38,7 @@ const Input: React.FC<Props> = ({
           type={type}
           name={name}
           onChange={onChange}
-          {...(value ? { value } : {})}
+          {...(value != undefined ? { value } : {})}
           className='p-2 bg-slate-50 block w-full border border-gray-300 rounded text-slate-800 py-2 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm'
         />
         {touched && error && (
