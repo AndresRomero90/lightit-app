ffcb06c2b0ab99e8da88059fdd99d1fa2ca8e828
Andres Romero <andres.romero.90@hotmail.com>
2023-07-10 11:24:48 -0300

    🐛 Fix AccuracyIndicator typing

--- src/diagnosis/components/AccuracyIndicator.tsx
+++ src/diagnosis/components/AccuracyIndicator.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 interface Props {
-  accuracy: number;
+  accuracy: string;
 }
 
 export const AccuracyIndicator: React.FC<Props> = ({ accuracy }) => {
   return (
     <div
       className='bg-slate-200 h-6 rounded-lg overflow-hidden border border-slate-400'
-      title={`${accuracy.toFixed(2)}%`}>
+      title={`${Number(accuracy).toFixed(2)}%`}>
       <div
         className='h-full bg-blue-600 shadow-lg'
         style={{ width: `${accuracy}%` }}></div>
