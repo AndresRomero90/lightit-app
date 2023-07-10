d775842abb943eb77a73bb9b1163ecc49287fda8
Andres Romero <andres.romero.90@hotmail.com>
2023-07-09 23:41:05 -0300

    ⚡️ Updated routes

--- src/shared/components/NavigationRouter.tsx
+++ src/shared/components/NavigationRouter.tsx
@@ -1,20 +1,26 @@
 import { lazy } from 'react';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { PrivateRoute } from '../../auth/components/PrivateRoute';
-const TestPage = lazy(() => import('../pages/TestPage'));
+
 const LoginPage = lazy(() => import('../../auth/pages/LoginPage'));
 const RegisterPage = lazy(() => import('../../auth/pages/RegisterPage'));
+const DiagnosisPage = lazy(() => import('../../diagnosis/pages/DiagnosisPage'));
+const UserHistoryPage = lazy(
+  () => import('../../diagnosis/pages/UserHistoryPage')
+);
 
 export const LOGIN_ROUTE = '/login';
 export const REGISTER_ROUTE = '/register';
 export const DIAGNOSIS_ROUTE = '/diagnosis';
+export const USER_HISTORY_ROUTE = '/history';
 
 export const NavigationRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<PrivateRoute />}>
-          <Route path={DIAGNOSIS_ROUTE} element={<TestPage />} />
+          <Route path={DIAGNOSIS_ROUTE} element={<DiagnosisPage />} />
+          <Route path={USER_HISTORY_ROUTE} element={<UserHistoryPage />} />
         </Route>
         <Route path={LOGIN_ROUTE} element={<LoginPage />} />
         <Route path={REGISTER_ROUTE} element={<RegisterPage />} />
