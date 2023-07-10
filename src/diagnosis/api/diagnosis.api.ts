4cfde1e464f4f67b80ccf5a56cd8ec65b348443f
Andres Romero <andres.romero.90@hotmail.com>
2023-07-10 08:59:07 -0300

    ⚡️ Updated Diagnosis api

--- src/diagnosis/api/diagnosis.api.ts
+++ src/diagnosis/api/diagnosis.api.ts
@@ -1,9 +1,11 @@
 import { AuthHttpService } from '../../auth/services/auth-http-service';
+import { DiagnosisResponse } from '../interfaces/DiagnosisResponse';
 import { SymptomsResponse } from '../interfaces/SymptomsResponse';
 
 export class DiagnosisApi {
   private static myInstance: DiagnosisApi | null = null;
   private static SYMPTOMS_BASE_URL = '/symptoms';
+  private static DIAGNOSIS_BASE_URL = '/diagnosis';
 
   static getInstance() {
     if (this.myInstance === null) {
@@ -12,13 +14,22 @@ export class DiagnosisApi {
     return this.myInstance;
   }
 
-  get callApiService() {
+  get diagnosisApiService() {
     return AuthHttpService.getInstance().api();
   }
 
   async getSymptoms() {
-    return await this.callApiService.get<SymptomsResponse>(
+    return await this.diagnosisApiService.get<SymptomsResponse>(
       DiagnosisApi.SYMPTOMS_BASE_URL
+    );
+  }
+
+  async getDiagnosis(symptoms: number[]) {
+    return await this.diagnosisApiService.post<DiagnosisResponse>(
+      DiagnosisApi.DIAGNOSIS_BASE_URL,
+      {
+        symptoms,
+      }
     );
   }
 }
