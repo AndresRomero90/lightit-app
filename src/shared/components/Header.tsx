69fd74580f62c8af9cd11bdbff123f8ffca95e5a
Andres Romero <andres.romero.90@hotmail.com>
2023-07-09 23:42:32 -0300

    ⚡️ Updated Header routes

--- src/shared/components/Header.tsx
+++ src/shared/components/Header.tsx
@@ -2,15 +2,19 @@ import { useNavigate } from 'react-router-dom';
 import { AuthApi } from '../../auth/api/auth.api';
 import Button from './Button';
 import { NavLink } from './NavLink';
-import { LOGIN_ROUTE } from './NavigationRouter';
+import {
+  DIAGNOSIS_ROUTE,
+  LOGIN_ROUTE,
+  USER_HISTORY_ROUTE,
+} from './NavigationRouter';
 
 export const Header: React.FC = () => {
   const navigate = useNavigate();
   return (
     <header className='w-full bg-blue-200 py-4'>
       <nav className='flex flex-row justify-center gap-8 items-center'>
-        <NavLink to='/diagnosis'>Diagnosis</NavLink>
-        <NavLink to='/history'>User History</NavLink>
+        <NavLink to={DIAGNOSIS_ROUTE}>Diagnosis</NavLink>
+        <NavLink to={USER_HISTORY_ROUTE}>User History</NavLink>
         <Button
           type='button'
           label='Log out'
